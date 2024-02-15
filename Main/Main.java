5f4bac7949401900090ae821f7baf13719731774
K.B CHOI <chlrudqh97@naver.com>
2024-02-15 15:35:17 +0900

    백준 심화 1 풀이

--- Main/Main.java
+++ Main/Main.java
@@ -10,30 +10,8 @@ public class Main {
         a.append(sc.next());
         sc.close();
 
-        int b = 0;
-       
-        for(int i = 0; i<a.length(); i++){
-            if(65<=(int)a.charAt(i) && (int)a.charAt(i)<=67){
-                b+=3;
-            }else if(68<=(int)a.charAt(i) && (int)a.charAt(i)<=70){
-                b+=4;
-            }else if(71<=(int)a.charAt(i) && (int)a.charAt(i)<=73){
-                b+=5;
-            }else if(74<=(int)a.charAt(i) && (int)a.charAt(i)<=76){
-                b+=6;
-            }else if(77<=(int)a.charAt(i) && (int)a.charAt(i)<=79){
-                b+=7;
-            }else if(80<=(int)a.charAt(i) && (int)a.charAt(i)<=83){
-                b+=8;
-            }else if(84<=(int)a.charAt(i) && (int)a.charAt(i)<=86){
-                b+=9;
-            }else if(87<=(int)a.charAt(i) && (int)a.charAt(i)<=90){
-                b+=10;
-            }
-
-            
-        }
-        System.out.println(b);
+        
+        System.out.println(a);
        
     }
 }
