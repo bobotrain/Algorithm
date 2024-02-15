fa4b775e6267c1a68919092f81b5334967c64af4
K.B CHOI <chlrudqh97@naver.com>
2024-02-15 16:10:55 +0900

    백준 심화1 풀이

---
+++ Main/Main2941.java
@@ -0,0 +1,20 @@
+package Main;
+
+import java.util.Scanner;
+ 
+public class Main2941 {
+ 
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String str[] = {"c=", "c-", "dz=", "d-", "lj", "nj", "s=", "z="};
+		
+		String st = sc.next();
+		
+		for(int i=0; i<str.length; i++) {	// 배열의 길이
+			if(st.contains(str[i])) // contains() : 해당 문자열이 존재하면 true 아니면 false
+				st = st.replace(str[i], "!");	// replace([기존문자], [바꿀문자])
+		}
+		
+		System.out.println(st.length());
+	}
+}
